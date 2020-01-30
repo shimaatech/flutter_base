263d00d98827503fceb2dd3b7b46c1df13619b74
morad <morad@shimaatech.com>
2020-01-30 21:29:28 +0200

    changed locator to protected member in AppContext

--- lib/src/app/app_context.dart
+++ lib/src/app/app_context.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 import '../../flutter_base.dart';
 
 abstract class BaseAppContext {
 
-  final Locator _locator = KiwiLocator();
+  @protected
+  final Locator locator = KiwiLocator();
 
-  T locate<T>([String name]) => _locator.locate<T>(name);
+  T locate<T>([String name]) => locator.locate<T>(name);
 
   Future<void> setup();
 
