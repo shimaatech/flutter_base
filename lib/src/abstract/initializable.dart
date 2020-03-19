23453cf53a70708464747100040d9c00aa86f0a7
morad <morad@shimaatech.com>
2020-03-19 23:12:46 +0200

    Some enhancements to Initializable

--- lib/src/abstract/initializable.dart
+++ lib/src/abstract/initializable.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_base/src/mixins/disposable.dart';
 
-
 /// Initialized directly when constructed
 abstract class Initializable with Disposable {
-
   Future<void> _initFuture;
 
   bool _initialized = false;
+
   bool get initialized => _initialized;
 
-
   @protected
-  Future<void> initialize();
+  Future<void> doInitialize();
 
-  Future<void> _initialize() async {
-    await initialize();
+  Future<void> initialize() async {
+    await _initFuture;
     _initialized = true;
   }
 
-  Future<void> ensureInitialized() => _initFuture;
+  void ensureInitialized() {
+    if (!initialized) {
+      throw NotInitializedException();
+    }
+  }
 
   Initializable() {
     // initialize directly when constructing the service
-    _initFuture = _initialize();
+    _initFuture = doInitialize();
   }
 
   @mustCallSuper
-  Future<void> dispose() async {
-  }
+  Future<void> dispose() async {}
+
+}
+
+
+class NotInitializedException implements Exception {
+
+  final String message;
+  NotInitializedException([this.message]);
+
 }
